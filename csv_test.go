cc81bc6829f419bdcd97e18ba3194867a1fd14a4
Louis Chemineau <artonge.c@gmail.com>
2017-06-19 13:04:48 +0300

    First commit

---
+++ csv_test.go
@@ -0,0 +1,155 @@
+package gocsv
+
+import "testing"
+
+type Test struct {
+	Name string  `csv:"header1"`
+	ID   int     `csv:"header2"`
+	Num  float64 `csv:"header3"`
+}
+
+type TestNoID struct {
+	Name string `csv:"header1"`
+	ID   int
+	Num  float64 `csv:"header"`
+}
+
+// Check the values are correct
+func checkValues(tabT []Test) bool {
+	return false ||
+		tabT[0].Name != "line1" || tabT[0].ID != 1 || tabT[0].Num != 1.2 ||
+		tabT[1].Name != "line2" || tabT[1].ID != 2 || tabT[1].Num != 2.3 ||
+		tabT[2].Name != "line3" || tabT[2].ID != 3 || tabT[2].Num != 3.4
+}
+
+func TestLoad_valideFile(t *testing.T) {
+	tabT := []Test{}
+	err := Load(Config{
+		path: "csv_files/valid.csv",
+		dest: &tabT,
+	})
+	if err != nil || checkValues(tabT) {
+		t.Fail()
+	}
+}
+
+func TestLoad_missATag(t *testing.T) {
+	tabT := []TestNoID{}
+	err := Load(Config{
+		path: "csv_files/valid.csv",
+		dest: &tabT,
+	})
+	if err != nil ||
+		tabT[0].Name != "line1" || tabT[0].ID != 0 || tabT[0].Num != 0 ||
+		tabT[1].Name != "line2" || tabT[1].ID != 0 || tabT[1].Num != 0 ||
+		tabT[2].Name != "line3" || tabT[2].ID != 0 || tabT[2].Num != 0 {
+		t.Fail()
+	}
+}
+
+func TestLoad_emptyFile(t *testing.T) {
+	tabT := []Test{}
+	err := Load(Config{
+		path: "csv_files/empty.csv",
+		dest: &tabT,
+	})
+	if err != nil || len(tabT) != 0 {
+		t.Fail()
+	}
+}
+
+func TestLoad_noHeader(t *testing.T) {
+	tabT := []Test{}
+	err := Load(Config{
+		path:   "csv_files/noHeader.csv",
+		dest:   &tabT,
+		header: []string{"header1", "header2", "header3"},
+	})
+	if err != nil || checkValues(tabT) {
+		t.Fail()
+	}
+}
+
+func TestLoad_withSemicolon(t *testing.T) {
+	tabT := []Test{}
+	err := Load(Config{
+		path:      "csv_files/semicolon.csv",
+		dest:      &tabT,
+		separator: ';',
+	})
+	if err != nil || checkValues(tabT) {
+		t.Fail()
+	}
+}
+
+func TestLoad_badFormat(t *testing.T) {
+	tabT := []Test{}
+	err := Load(Config{
+		path: "csv_files/badFormat.csv",
+		dest: &tabT,
+	})
+	if err == nil {
+		t.Fail()
+	}
+}
+
+func TestLoad_nonexistingFile(t *testing.T) {
+	tabT := []Test{}
+	err := Load(Config{
+		path: "csv_files/nonexistingfile.csv",
+		dest: &tabT,
+	})
+	if err == nil {
+		t.Fail()
+	}
+}
+
+func TestLoad_badInt(t *testing.T) {
+	tabT := []Test{}
+	err := Load(Config{
+		path: "csv_files/badInt.csv",
+		dest: &tabT,
+	})
+	if err == nil {
+		t.Fail()
+	}
+}
+
+func TestLoad_badFloat(t *testing.T) {
+	tabT := []Test{}
+	err := Load(Config{
+		path: "csv_files/badFloat.csv",
+		dest: &tabT,
+	})
+	if err == nil {
+		t.Fail()
+	}
+}
+
+func TestLoad_notPath(t *testing.T) {
+	err := Load(Config{
+		dest: &[]Test{},
+	})
+	if err == nil {
+		t.Fail()
+	}
+}
+
+func TestLoad_noDest(t *testing.T) {
+	err := Load(Config{
+		path: "csv_files/valid.csv",
+	})
+	if err == nil {
+		t.Fail()
+	}
+}
+
+func TestLoad_noDist(t *testing.T) {
+	err := Load(Config{
+		path: "csv_files/valid.csv",
+		dest: &Test{},
+	})
+	if err == nil {
+		t.Fail()
+	}
+}
---
+++ go-csv.go
@@ -0,0 +1,169 @@
+package gocsv
+
+import (
+	"encoding/csv"
+	"fmt"
+	"os"
+	"reflect"
+	"strconv"
+)
+
+// Config struct to pass to the Load function
+type Config struct {
+	path      string
+	dest      interface{}
+	separator rune
+	header    []string
+}
+
+// Load - Load a csv file and put it in a array of the dest type
+// This uses tags
+// Exemple:
+// 	tabT := []Test{}
+// 	err  := Load(Config{
+// 		path: "csv_files/valid.csv",
+// 		dest: &tabT,
+// 		separator: ';',
+// 		header: []string{"header1", "header2", "header3"}
+// 	})
+// The 'separator' and 'header' properties of the config object are optionals
+// @param dest: object where to store the result
+// @return an error if one occure
+func Load(config Config) error {
+	header, content, err := readFile(config.path, config.separator, config.header)
+	if err != nil {
+		return fmt.Errorf("Error while loading csv (%v):\n	==> %v", config.path, err)
+	}
+	// This means that the file is empty
+	if content == nil {
+		return nil
+	}
+	// If there is some header in the config, don't skip the first line
+	start := 1
+	if config.header != nil {
+		start = 0
+	}
+	// Map content to the destination
+	err = mapToDest(header, content[start:], config.dest)
+	if err != nil {
+		return fmt.Errorf("Error while mapping the content to the destination:\n	==> %v", err)
+	}
+	return nil
+}
+
+// Load the header and file content in memory
+// @param path: path of the csv file
+// @param separator: the separator used in the csv file
+// @param header: the optional header if the file does not contain one
+func readFile(path string, separator rune, header []string) (map[string]int, [][]string, error) {
+	// Open the file
+	file, err := os.Open(path)
+	if err != nil {
+		return nil, nil, err
+	}
+	// Read the file
+	// We need to read it all at once to have the number of records
+	reader := csv.NewReader(file) // Create the csv reader
+	reader.TrimLeadingSpace = true
+	if separator != 0 {
+		reader.Comma = separator
+	}
+	content, err := reader.ReadAll() // Read all the file and put it in content
+	file.Close()                     // Close the file
+	if err != nil {
+		return nil, nil, err
+	}
+	// If file is empty, return
+	if len(content) == 0 {
+		return nil, nil, nil
+	}
+	// Map each header name to its index
+	// This will be used in the mapping
+	// If there is no header in the config, treate first line as the header
+	rawHeader := header
+	if rawHeader == nil {
+		rawHeader = content[0]
+	}
+	headerMap := make(map[string]int) // Create map
+	for i, name := range rawHeader {
+		headerMap[name] = i
+	}
+	// Return our header and content
+	return headerMap, content, nil
+}
+
+// Map the provided content to the dest using the header and the tags
+// @param header: the csv header to match with the struct's tags
+// @param content: the content to put in dest
+// @param dest: the destination where to put the file's content
+func mapToDest(header map[string]int, content [][]string, dest interface{}) error {
+	// Check destination is not nit
+	if dest == nil {
+		return fmt.Errorf("Destination slice is nil")
+	}
+	// Check destination is a slice
+	if reflect.TypeOf(dest).Elem().Kind() != reflect.Slice {
+		return fmt.Errorf("Destination is not a slice")
+	}
+	// Create the slice the put the values in
+	// Get the reflected value of dest
+	destRv := reflect.ValueOf(dest).Elem()
+	// Create a new reflected value containing a slice:
+	//   type    : dest's type
+	//   length  : content's length
+	//   capacity: content's length
+	sliceRv := reflect.MakeSlice(destRv.Type(), len(content), len(content))
+	// Map the records into the created slice
+	for i, record := range content {
+		item := sliceRv.Index(i) // Get the ieme item from the slice
+		// Map all fields into the item
+		for j := 0; j < item.NumField(); j++ {
+			fieldTag := item.Type().Field(j).Tag.Get("csv") // Get the tag of the jeme field of the struct
+			if fieldTag == "" {
+				continue
+			}
+			rflFld := item.Field(j) // Get the reflected value of the field
+			fieldPos, ok := header[fieldTag]
+			if !ok {
+				continue
+			}
+			valStr := record[fieldPos]        // Get the value from the record
+			err := storeValue(valStr, rflFld) // Store the value in the reflected field
+			if err != nil {
+				return fmt.Errorf("(record %v to the slice %v):\n	==> %v", record, item, err)
+			}
+		}
+	}
+	// Set destRv to be sliceRv
+	destRv.Set(sliceRv)
+	return nil
+}
+
+// Set the value of the rflVal to valStr
+// Make some parsing if needed
+// @param valStr: the value, as a string, that we want to store
+// @param rflVal: the reflected value where we want to store our value
+// @return an error if one occure
+func storeValue(valStr string, rflVal reflect.Value) error {
+	switch rflVal.Kind() {
+	case reflect.String:
+		rflVal.SetString(valStr)
+	case reflect.Int:
+		// Parse the value to an int
+		value, err := strconv.ParseInt(valStr, 10, 64)
+		if err != nil {
+			return fmt.Errorf("Error while parsing int %v:\n	==> %v", valStr, err)
+
+		}
+		rflVal.SetInt(value)
+	case reflect.Float64:
+		// Parse the value to an float
+		value, err := strconv.ParseFloat(valStr, 64)
+		if err != nil {
+			return fmt.Errorf("Error while parsing float %v:\n	==> %v", valStr, err)
+		}
+		rflVal.SetFloat(value)
+	}
+
+	return nil
+}
