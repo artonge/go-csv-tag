14ca5fd2f9e8b6a73e5991be442903998a6def34
chenall <chenall.cn@gmail.com>
2024-05-13 11:30:37 +0800

    New CsvOption - UseCRLF True to use \r\n as the line terminator

--- csvtag.go
+++ csvtag.go
@@ -3,6 +3,7 @@ package csvtag
 // CsvOptions - options when loading or dumping csv.
 type CsvOptions struct {
 	Separator rune
+	UseCRLF   bool // True to use \r\n as the line terminator
 	Header    []string
 	TagKey    string
 }
